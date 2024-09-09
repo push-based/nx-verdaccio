7704a45a478b40036db5268ab631a3906f45aa6e
Michael <michael.hladky@push-based.io>
2024-09-09 17:52:52 +0300

    add setup tests

--- tooling/build-env/src/shared/setup.spec.ts
+++ tooling/build-env/src/shared/setup.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { getEnvironmentsRoot } from '@org/build-env';
+import { getEnvironmentRoot, getEnvironmentsRoot } from '@org/build-env';
 
 describe('getEnvironmentsRoot', () => {
   it('should return default env dir', () => {
@@ -8,5 +8,24 @@ describe('getEnvironmentsRoot', () => {
 
   it('should return given path', () => {
     expect(getEnvironmentsRoot('env')).toBe('env');
+  });
+});
+
+describe('getEnvironmentRoot', () => {
+  //  projectName = process.env['NX_TASK_TARGET_PROJECT'],
+  //   environmentsDir?: string
+  it('should return current running tasks project name by default', () => {
+    //process.env['NX_TASK_TARGET_PROJECT'] = 'cli-e2e';
+    expect(getEnvironmentRoot()).toMatch(/build-env$/);
+  });
+
+  it('should accept project name', () => {
+    expect(getEnvironmentRoot('cli-e2e')).toMatch(/cli-e2e$/);
+  });
+
+  it('should accept environments root', () => {
+    expect(getEnvironmentRoot('cli-e2e', 'tmp/test-env')).toMatch(
+      /^tmp\/test-env/
+    );
   });
 });
