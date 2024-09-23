55d3f820a72dfeb5da645d3ef4565ce44d0d16e6
Michael <michael.hladky@push-based.io>
2024-09-23 20:20:33 +0200

    add comments

--- projects/build-env/src/executors/npm-install/executor.ts
+++ projects/build-env/src/executors/npm-install/executor.ts
@@ -35,9 +35,7 @@ export default async function runNpmInstallExecutor(
       _: ['install', `${packageNameAndVersion}`],
       fund: false, // avoid polluted terminal
       shrinkwrap: false, // avoid package-lock creation or update
-      save: true,
-      // prefix: environmentRoot,
-      // userconfig: join(environmentRoot, NPMRC_FILENAME),
+      save: true, // save to package.json dependencies
     }),
     cwd: environmentRoot,
     verbose: true,
