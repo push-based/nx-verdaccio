21151ee1486729c8c83049e8014385d3023b4b76
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2024-10-13 10:44:04 +0200

    Apply suggestions from code review

--- projects/nx-verdaccio/src/executors/env-teardown/git.ts
+++ projects/nx-verdaccio/src/executors/env-teardown/git.ts
@@ -2,11 +2,9 @@ import {simpleGit} from 'simple-git';
 
 export async function isFolderInGit(folderPath: string): Promise<boolean> {
   try {
-    // Initialize simple-git with the folder path
     const git = simpleGit(folderPath);
     // Check if the folder is a git repository
     const isRepo = (await git.checkIgnore(folderPath)).length === 0;
-    // console.log(`${folderPath} is ${isRepo ? '' : 'not '} in Git repository.`);
     return isRepo;
   } catch (error) {
     if (
