a2881379467be4ea866d604c59938cb340d0a265
Michael <michael.hladky@push-based.io>
2024-10-13 10:22:22 +0200

    wip

--- projects/nx-verdaccio/src/executors/env-teardown/git.ts
+++ projects/nx-verdaccio/src/executors/env-teardown/git.ts
@@ -1,6 +1,5 @@
 import type { SimpleGit } from 'simple-git';
 import { simpleGit } from 'simple-git';
-import { resolve } from 'node:path';
 
 export const gitClient: SimpleGit = simpleGit();
 
@@ -29,31 +28,6 @@ export async function isFolderInRepo(folderPath: string): Promise<boolean> {
       return true;
     }
     console.log(`${error}`);
-    return false;
-  }
-}
-
-export async function isSubfolderInGitRepository(
-  subFolderPath: string,
-  baseFolderPath = process.cwd()
-) {
-  // Resolve the full path for the subfolder
-  const fullSubFolderPath = resolve(baseFolderPath, subFolderPath);
-
-  // Initialize simple-git with the full subfolder path
-  const git = simpleGit(fullSubFolderPath);
-
-  try {
-    // Check if the subfolder path is within a Git repository
-    const isRepo = await git.checkIsRepo();
-    if (isRepo) {
-      console.log(`${fullSubFolderPath} is inside a Git repository.`);
-    } else {
-      console.log(`${fullSubFolderPath} is not inside a Git repository.`);
-    }
-    return isRepo;
-  } catch (error) {
-    console.log(`${fullSubFolderPath} is not inside a Git repository.`);
     return false;
   }
 }
