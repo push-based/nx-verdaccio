ff52749b6df1d76d2aebf43048aec9ec8c48eacf
Michael <michael.hladky@push-based.io>
2024-10-23 18:15:19 +0200

    fix: add skipTeardown to env-teardown executor

--- projects/nx-verdaccio/src/executors/env-teardown/schema.ts
+++ projects/nx-verdaccio/src/executors/env-teardown/schema.ts
@@ -1,7 +1,8 @@
-import { type Environment } from '../env-bootstrap/npm';
+import {type Environment} from '../env-bootstrap/npm';
 
 export type TeardownExecutorOptions = Partial<
   Environment & {
-    verbose: boolean;
-  }
+  verbose: boolean;
+  skipTeardown: boolean;
+}
 >;
