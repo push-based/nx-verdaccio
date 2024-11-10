f187b57fc36dc1a479c7e8525004329105a0c977
Michael <michael.hladky@push-based.io>
2024-11-10 02:53:05 +0100

    wip CP

--- code-pushup.config.ts
+++ code-pushup.config.ts
@@ -6,20 +6,21 @@ import nxPerformancePlugin, {
 import {TaskTimeAuditOption} from './tooling/measures/nx-performance';
 
 const onlyAudits: OnlyAudit[] = [
-  'graph-time-project',
-  'graph-time-task',
-  'cache-size',
+ // 'graph-time-project',
+ // 'graph-time-task',
+ // 'cache-size',
   'task-time',
 ];
 const taskGraphTasks = ['cli-e2e:nxv-env-install'];
 const taskTimeTasks: TaskTimeAuditOption[] = [
-  {task: 'models-e2e:nxv-env-teardown'},
-  {task: 'models-e2e:nxv-env-bootstrap'},
-  {task: 'models-e2e:nxv-env-setup'},
   {task: 'models-e2e:e2e'},
-  {task: 'models-e2e:nxv-e2e'},
-  {task: 'nx-verdaccio-e2e:nxv-e2e'},
-  {task: 'cli-e2e-original:original-e2e', options: { exclude: ["nx-verdaccio"]}},
+  /* {task: 'models-e2e:nxv-env-teardown'},
+   {task: 'models-e2e:nxv-env-bootstrap'},
+   {task: 'models-e2e:nxv-env-setup'},
+   {task: 'models-e2e:e2e'},
+   {task: 'models-e2e:nxv-e2e'},
+   {task: 'nx-verdaccio-e2e:nxv-e2e'},
+   {task: 'cli-e2e-original:original-e2e', options: { exclude: ["nx-verdaccio"]}},*/
 ];
 const cacheSizeTasks = [
   'models-e2e:nxv-env-setup',
