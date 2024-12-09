35c5b02252f7cb315aa88fb7463f89ab225ca9ff
Michael <michael.hladky@push-based.io>
2024-12-09 20:40:14 +0100

    wip

---
+++ org/src/lib/org.spec.ts
@@ -0,0 +1,7 @@
+import { org } from './org';
+
+describe('org', () => {
+  it('should work', () => {
+    expect(org()).toEqual('org');
+  });
+});
---
+++ org/src/lib/org.ts
@@ -0,0 +1,3 @@
+export function org(): string {
+  return 'org';
+}
