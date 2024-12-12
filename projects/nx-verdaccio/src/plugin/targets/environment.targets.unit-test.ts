0446e1df60a69120e9f90aa2654cedacbd6548c1
adrianromanski <adrian.romanski@codeweaver.pl>
2024-12-12 19:29:16 +0100

    tests: add tests structure

---
+++ projects/nx-verdaccio/src/plugin/targets/environment.targets.unit-test.ts
@@ -0,0 +1,61 @@
+import { describe } from 'vitest';
+
+describe('isEnvProject', () => {
+  //export function isEnvProject(
+  //   projectConfig: ProjectConfiguration,
+  //   options: NormalizedCreateNodeOptions['environments']
+  // ): boolean {
+  //   const { tags: existingTags = [], targets } = projectConfig;
+  //   const existingTargetNames = Object.keys(targets ?? {});
+  //   const {
+  //     filterByTags: environmentsTagFilters,
+  //     targetNames: environmentTargetNames,
+  //   } = options;
+  //
+
+
+  //   if (!existingTargetNames || !environmentTargetNames) {
+  //     return false;
+  //   }
+  //
+  //   if (
+  //     existingTargetNames.some((existingTarget) =>
+  //       environmentTargetNames.includes(existingTarget)
+  //     )
+  //   ) {
+  //     if (existingTags && environmentsTagFilters) {
+  //       return existingTags.some((existingTag) =>
+  //         environmentsTagFilters.includes(existingTag)
+  //       );
+  //     }
+  //     return true;
+  //   }
+  //
+  //   return false;
+  // }
+
+  //   IT test
+  //   if (!existingTargetNames || !environmentTargetNames) {
+  //     return false;
+  //   }
+
+       // IT test
+  //   if (
+  //     existingTargetNames.some((existingTarget) =>
+  //       environmentTargetNames.includes(existingTarget)
+  //     )
+  //   ) {
+         // IT test
+  //     if (existingTags && environmentsTagFilters) {
+  //       return existingTags.some((existingTag) =>
+  //         environmentsTagFilters.includes(existingTag)
+  //       );
+  //     }
+
+  //     return true;
+  //   }
+        // IT test
+  //   //   return false;
+
+
+})
