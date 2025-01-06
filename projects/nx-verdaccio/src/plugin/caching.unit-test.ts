1af8d1cbed834506e90b0e1eb962c08f83175f79
adrianromanski <adrian.romanski@codeweaver.pl>
2025-01-06 09:50:57 +0100

    tests: change names of describe blocks to match function names

--- projects/nx-verdaccio/src/plugin/caching.unit-test.ts
+++ projects/nx-verdaccio/src/plugin/caching.unit-test.ts
@@ -8,8 +8,8 @@ import {
 import { type JsonWriteOptions, type JsonReadOptions } from 'nx/src/utils/fileutils';
 import {
   getCacheRecord,
-  readTargetsCache,
   setCacheRecord,
+  readTargetsCache,
   writeTargetsToCache,
 } from './caching';
 
@@ -17,14 +17,13 @@ import * as nodeFs from 'node:fs';
 import * as nxDevKit from '@nx/devkit';
 import * as cachingUtils from './utils/caching.utils';
 
+const PREFIX = 'warcraft';
+const CACHE_KEY = 'ragnaros';
 const PATH = 'azeroth';
 const MOCK_CACHE_ITEM = { name: 'mocked-name' };
 const MOCK_TARGET_CACHE = { ragnaros: MOCK_CACHE_ITEM };
 
-describe('cacheRecord', (): void => {
-  const prefix = 'warcraft';
-  const cacheKey = 'ragnaros';
-
+describe('getCacheRecord', (): void => {
   let cacheKeySpy: MockInstance<
     [prefix: string, MOCK_CACHE_ITEM: Record<string, unknown>],
     string
@@ -33,42 +32,58 @@ describe('cacheRecord', (): void => {
   beforeEach((): void => {
     cacheKeySpy = vi
       .spyOn(cachingUtils, 'cacheKey')
-      .mockReturnValue(cacheKey);
+      .mockReturnValue(CACHE_KEY);
   });
   afterEach((): void => {
     cacheKeySpy.mockRestore();
   });
 
   it('should call cacheKey once with correct arguments', (): void => {
-    getCacheRecord(MOCK_TARGET_CACHE, prefix, MOCK_CACHE_ITEM);
+    getCacheRecord(MOCK_TARGET_CACHE, PREFIX, MOCK_CACHE_ITEM);
     expect(cacheKeySpy).toHaveBeenCalledTimes(1);
-    expect(cacheKeySpy).toHaveBeenCalledWith(prefix, MOCK_CACHE_ITEM);
+    expect(cacheKeySpy).toHaveBeenCalledWith(PREFIX, MOCK_CACHE_ITEM);
   });
 
   it('should return the correct cache record if there is a cache hit', (): void => {
-    expect(getCacheRecord(MOCK_TARGET_CACHE, prefix, MOCK_CACHE_ITEM)).toEqual(MOCK_CACHE_ITEM);
+    expect(getCacheRecord(MOCK_TARGET_CACHE, PREFIX, MOCK_CACHE_ITEM)).toEqual(MOCK_CACHE_ITEM);
   });
 
   it('should return undefined if there is no cache hit', (): void => {
     cacheKeySpy.mockReturnValue('non-existent-key');
-    expect(getCacheRecord(MOCK_TARGET_CACHE, prefix, MOCK_CACHE_ITEM)).toBeUndefined();
+    expect(getCacheRecord(MOCK_TARGET_CACHE, PREFIX, MOCK_CACHE_ITEM)).toBeUndefined();
+  });
+});
+
+describe('setCacheRecord', (): void => {
+  let cacheKeySpy: MockInstance<
+    [prefix: string, MOCK_CACHE_ITEM: Record<string, unknown>],
+    string
+  >;
+
+  beforeEach((): void => {
+    cacheKeySpy = vi
+      .spyOn(cachingUtils, 'cacheKey')
+      .mockReturnValue(CACHE_KEY);
+  });
+  afterEach((): void => {
+    cacheKeySpy.mockRestore();
   });
 
   it('should set the cache record, and return it', (): void => {
-    expect(setCacheRecord(MOCK_TARGET_CACHE, prefix, MOCK_CACHE_ITEM, MOCK_CACHE_ITEM)).toBe(
-      MOCK_CACHE_ITEM
-    );
-    expect(MOCK_TARGET_CACHE).toHaveProperty(cacheKey, MOCK_CACHE_ITEM);
+    expect(
+      setCacheRecord(MOCK_TARGET_CACHE, PREFIX, MOCK_CACHE_ITEM, MOCK_CACHE_ITEM)
+    ).toBe(MOCK_CACHE_ITEM);
+    expect(MOCK_TARGET_CACHE).toHaveProperty(CACHE_KEY, MOCK_CACHE_ITEM);
   });
 
   it('should update existing cache data, and return it', (): void => {
     const recordToUpdate = { name: 'Soul of Sylvanas' };
 
-    setCacheRecord(MOCK_TARGET_CACHE, prefix, MOCK_CACHE_ITEM, MOCK_CACHE_ITEM);
-    expect(setCacheRecord(MOCK_TARGET_CACHE, prefix, MOCK_CACHE_ITEM, recordToUpdate)).toBe(
-      recordToUpdate
-    );
-    expect(MOCK_TARGET_CACHE).toHaveProperty(cacheKey, recordToUpdate);
+    setCacheRecord(MOCK_TARGET_CACHE, PREFIX, MOCK_CACHE_ITEM, MOCK_CACHE_ITEM);
+    expect(
+      setCacheRecord(MOCK_TARGET_CACHE, PREFIX, MOCK_CACHE_ITEM, recordToUpdate)
+    ).toBe(recordToUpdate);
+    expect(MOCK_TARGET_CACHE).toHaveProperty(CACHE_KEY, recordToUpdate);
   });
 });
 
