c0adb816553cf73ce7b2d1c3d9ad9f5585556a33
adrianromanski <adrian.romanski@codeweaver.pl>
2025-01-08 16:51:55 +0100

    tests: add normalizeCreateNodesOptions spy and test

--- projects/nx-verdaccio/src/plugin/targets/create-targets.unit-test.ts
+++ projects/nx-verdaccio/src/plugin/targets/create-targets.unit-test.ts
@@ -1,12 +1,55 @@
-import { describe } from 'vitest';
+import { beforeEach, describe, MockInstance } from 'vitest';
+import { ProjectConfiguration } from '@nx/devkit';
+
+import { createProjectConfiguration } from './create-targets';
+
+import { NormalizedCreateNodeOptions } from '../normalize-create-nodes-options';
+import { NxVerdaccioCreateNodeOptions } from '../schema';
+
+import * as normalizeCreateNodesModule from './../normalize-create-nodes-options';
 
 describe('createProjectConfiguration', (): void => {
+  const config: ProjectConfiguration = {
+    root: 'mock-root',
+    name: 'unit-test-project',
+    targets: {  build: { executor: 'nx:build', options: {} },},
+    tags: ['env:production'],
+  };
 
-  // spies
-  //  const { environments, packages } = normalizeCreateNodesOptions(options);
-  it('should call normalizeCreateNodesOptions ones with options', (): void => {
+  const options: NormalizedCreateNodeOptions = {
+    environments: {
+      targetNames: ['build'],
+      environmentsDir: './environments',
+    },
+    packages: {
+      targetNames: ['test', 'lint'],
+      environmentsDir: './packages',
+      filterByTags: ['env:production', 'utility'],
+    },
+  };
 
+  let normalizeCreateNodesOptionsSpy: MockInstance<
+    [options: NxVerdaccioCreateNodeOptions],
+    NormalizedCreateNodeOptions
+  >;
+
+  beforeEach((): void => {
+    normalizeCreateNodesOptionsSpy = vi
+      .spyOn(normalizeCreateNodesModule, 'normalizeCreateNodesOptions')
+      .mockReturnValue(options)
+  })
+
+  afterEach((): void => {
+    normalizeCreateNodesOptionsSpy.mockRestore();
+  })
+
+  it('should call normalizeCreateNodesOptions ones with config and options', (): void => {
+    createProjectConfiguration(config, options);
+    expect(normalizeCreateNodesModule.normalizeCreateNodesOptions).toHaveBeenCalledWith(
+      options
+    );
   });
+
   //  const isE2eProject = isEnvProject(projectConfiguration, environments);
   it('should call normalizeCreateNodesOptions ones with projectConfiguration and environments', (): void => {
 
