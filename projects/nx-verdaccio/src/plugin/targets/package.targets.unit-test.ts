d9a12d0935f589c29f9e4e32db2279ee9a17d2a8
adrianromanski <adrian.romanski@codeweaver.pl>
2025-01-09 17:10:23 +0100

    tests: getPkgTargets object structure

--- projects/nx-verdaccio/src/plugin/targets/package.targets.unit-test.ts
+++ projects/nx-verdaccio/src/plugin/targets/package.targets.unit-test.ts
@@ -1,5 +1,14 @@
 import { describe, it, expect } from 'vitest';
-import { isPkgProject } from './package.targets';
+
+import {
+  getPkgTargets,
+  isPkgProject,
+  TARGET_PACKAGE_INSTALL,
+  TARGET_PACKAGE_PUBLISH,
+} from './package.targets';
+
+import { EXECUTOR_PACKAGE_NPM_INSTALL } from '../../executors/pkg-install/constants';
+import { EXECUTOR_PACKAGE_NPM_PUBLISH } from '../../executors/pkg-publish/constants';
 
 describe('isPkgProject', () => {
   it('should return true for projects with projectType is library', () => {
@@ -56,5 +65,33 @@ describe('isPkgProject', () => {
         {}
       )
     ).toBe(false);
+  });
+});
+
+describe('getPkgTargets', (): void => {
+  it('should generate PkgTargets with correct structure', (): void => {
+    const result = getPkgTargets();
+    expect(result).toMatchObject({
+      [TARGET_PACKAGE_PUBLISH]: expect.any(Object),
+      [TARGET_PACKAGE_INSTALL]: expect.any(Object),
+    });
+  });
+
+  it('should generate TARGET_PACKAGE_PUBLISH with correct structure', (): void => {
+    const result = getPkgTargets();
+    expect(result[TARGET_PACKAGE_PUBLISH]).toMatchObject({
+      dependsOn: expect.any(Array),
+      executor: expect.stringContaining(EXECUTOR_PACKAGE_NPM_PUBLISH),
+      options: expect.any(Object),
+    });
+  });
+
+  it('should generate TARGET_PACKAGE_INSTALL with correct structure', (): void => {
+    const result = getPkgTargets();
+    expect(result[TARGET_PACKAGE_INSTALL]).toMatchObject({
+      dependsOn: expect.any(Array),
+      executor: expect.stringContaining(EXECUTOR_PACKAGE_NPM_INSTALL),
+      options: expect.any(Object),
+    });
   });
 });
