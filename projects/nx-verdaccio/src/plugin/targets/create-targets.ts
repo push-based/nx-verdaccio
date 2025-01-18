5bcc40d88b6480e271d3799d8d696764b0fe2211
adrianromanski <adrian.romanski@codeweaver.pl>
2025-01-18 17:15:24 +0100

    docs: rephrasing, formatting

--- projects/nx-verdaccio/src/plugin/targets/create-targets.ts
+++ projects/nx-verdaccio/src/plugin/targets/create-targets.ts
@@ -10,22 +10,22 @@ import {
 import { getPkgTargets, isPkgProject } from './package.targets';
 
 /**
- * Generates a project configuration with targets and namedInputs.
+ * Generates a project configuration partial including `targets` and `namedInputs`.
  *
- * If the project is an environment project - `isEnvProject`(),
- * wraps the results of `verdaccioTargets()`, `getEnvTargets()`,
- *`updateEnvTargetNames()`, and namedInputs
+ * If the project is an environment project, derived by `isEnvProject()`,
+ * It returns the results of `verdaccioTargets()`, `getEnvTargets()`,
+ *`updateEnvTargetNames()` under `targets`, and `namedInputs`
  *
- * If the project is a publishable project - `isPkgProject()`,
- * wraps the results of `getPkgTargets()`, and namedInputs
+ * If the project is a publishable project, derived by `isPkgProject()`,
+ * it returns the results of `getPkgTargets()`, and `namedInputs`
  *
- * Otherwise, returns and empty object (early return)
+ * Otherwise, it returns an empty object (as early exit)
  *
- * Logs warnings for missing implicit dependencies in environment projects.
+ * Additionally, it logs warnings for missing implicit dependencies in environment projects.
  *
  * @param projectConfiguration
  * @param options
- * @returns A partial project configuration with targets and namedInputs.
+ * @returns A partial project configuration with `targets` and `namedInputs`.
  */
 export function createProjectConfiguration(
   projectConfiguration: ProjectConfiguration,
@@ -46,11 +46,10 @@ export function createProjectConfiguration(
   }
 
   /**
-   * Unfortunately namedInputs are not picked up by tasks graph: Error: Input 'build-artifacts' is not defined
-   * When you pass your own namedInputs (like you would in a project.json file) via the inferred tasks plugin,
-   * the tasks pipeline ignores them and throws this error.
-   * Some Nx plugins use the default namedInput, probably for that reason,
-   * but I'm concerned that if developers change those inputs, it might lead to undesired behavior.
+   * When you pass your own `namedInputs` (like you would in a `project.json` file)
+   * via the inferred tasks plugin, the tasks pipeline ignores them and throws this error.
+   * Some Nx plugins use the default `namedInput`, probably for that reason,
+   * but I'm concerned that if developers change those inputs, it might lead to undesired behaviour.
    * @todo investigate if there is a way to pass namedInputs to the tasks graph
    */
   const namedInputs: ProjectConfiguration['namedInputs'] = {
