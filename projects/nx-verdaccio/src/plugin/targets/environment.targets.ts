53ce615efd7e07866b34d552ee13582831c56941
Michael <michael.hladky@push-based.io>
2025-12-20 23:02:50 +0100

    fix: revert options, updated pkgs

--- projects/nx-verdaccio/src/plugin/targets/environment.targets.ts
+++ projects/nx-verdaccio/src/plugin/targets/environment.targets.ts
@@ -69,7 +69,6 @@ export function verdaccioTargets(
         clear: true,
         environmentDir,
         projectName: envProject,
-        verbose: true, // @TODO: make executor also work without verbose true. (needed to check if server is running)
       },
     },
     [inferredTargets.verdaccioStop]: {
